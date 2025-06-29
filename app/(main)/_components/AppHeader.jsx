ae285e704d0b6a2943f1c4b9f6eacf965ba505b3
dheerajcodes20 <dheeraj2082dk@gmail.com>
2025-06-29 17:03:07 +0530

    talktutor changes 11

--- app/(main)/_components/AppHeader.jsx
+++ app/(main)/_components/AppHeader.jsx
@@ -6,7 +6,7 @@ function AppHeader() {
   return (
     <div className='p-3 shadow-sm flex justify-between items-center bg-white '>
 
-     <div className='text-4xl font-bold text-black ml-4'>Talk<span className='text-blue-600'>Tutor</span></div>
+     <div className='text-4xl font-bold text-black ml-4'>ASK<span className='text-blue-600'>ly</span></div>
      <UserButton/>
     </div>
   )
